32c54367176e2980b17952f38536e5802d33b88d
Jason Schweier <jasons@nulogy.com>
2020-01-03 19:46:00 -0500

    P03 - kth element

--- src/ninety_nine_problems_clojure/core.clj
+++ src/ninety_nine_problems_clojure/core.clj
@@ -9,3 +9,8 @@
   "P02 - Find the last but one element of a list."
   [coll]
   (last (butlast coll)))
+
+(defn kth
+  "P03 - Find the K'th element of a list. The first element in the list is number 1."
+  [coll k]
+  (nth coll (- k 1)))
--- test/ninety_nine_problems_clojure/core_test.clj
+++ test/ninety_nine_problems_clojure/core_test.clj
@@ -6,8 +6,17 @@
   (is (= (my-last '(1 2 3)) 3) "a simple list")
   (is (= (my-last '()) nil) "an empty list"))
 
-(deftest my-last-test
+(deftest penultimate-test
   (is (= (penultimate '()) nil) "an empty list")
   (is (= (penultimate '(1)) nil) "list too short")
   (is (= (penultimate '(1 2)) 1) "shortest list")
   (is (= (penultimate '(1 2 3)) 2) "simple list"))
+
+(deftest kth-test
+  (is (thrown? java.lang.IndexOutOfBoundsException (kth '() 1)) "an empty list")
+  (is (thrown? java.lang.IndexOutOfBoundsException (kth '(1) 0)) "out of lower bounds")
+  (is (thrown? java.lang.IndexOutOfBoundsException (kth '(1) 2)) "out of upper bounds")
+  (is (= (kth '(1) 1) 1) "singleton list")
+  (is (= (kth '(:one :two :three) 1) :one) "simple list")
+  (is (= (kth '(:one :two :three) 2) :two) "simple list")
+  (is (= (kth '(:one :two :three) 3) :three) "simple list"))
